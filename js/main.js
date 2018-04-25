ce01bda6271fff0233f53c456728ec912dc9f835
kate <kate@yunjingit.com>
2018-04-25 18:47:44 +0800

    change

--- js/main.js
+++ js/main.js
@@ -19,5 +19,12 @@ $(document).ready(function() {
         $(".verify-pc").hide();
         $(".sign-pc-container").hide();
         $(".sign-app").show();
-    })
-})
+    });
+
+    $(".download-btn").mousedown(function() {
+        $(".download-btn").css("box-shadow","none");
+    });
+    $(".download-btn").mouseup(function() {
+        $(".download-btn").css("box-shadow","0 5px 10px rgba(75,46,0,0.2)");
+    });
+});
